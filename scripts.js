5b9211cf45215a005bccfa4a37c3f7599da30f0c
iamakashs <akash257.ht@gmail.com>
2022-11-18 17:19:57 +0530

    Project Completed

---
+++ scripts.js
@@ -0,0 +1,48 @@
+var todos =[ //we dont need this variable this wasa only for testing
+    {
+      text:'JavaScript',
+      done:true,
+    },
+    {
+        text:'Angular',
+        done:true,
+    },
+    {
+        text:'React',
+        done:false,
+    },
+
+]
+
+const toDoApp = {
+    data(){
+        return{
+            todos:window.todos,  //we should keep it empty like: "todos:[]" since we will be saving this in to database
+            newTodo:{
+                done:false,
+            },
+        }
+    },
+    methods: {
+        addItem:function(){
+            if(this.newTodo.text){
+
+                this.todos.push(this.newTodo);
+                this.newTodo = {
+                    done:false
+                };
+            }else{
+                alert("You left the input blank!!")
+            }
+        },
+        markComplete:function(){
+            
+        },
+        clearAll: function(){
+         this.todos = [];
+        },
+
+    },
+}
+
+Vue.createApp(toDoApp).mount('#app')
